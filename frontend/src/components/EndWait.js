0dbb08424db164f317c99be2941ebe1b78bd8696
Frank Wang <frankningwang@gmail.com>
2024-09-30 23:29:59 -0700

    new text

--- frontend/src/components/EndWait.js
+++ frontend/src/components/EndWait.js
@@ -4,7 +4,7 @@ function EndWait() {
   return (
     <div className="generalInstructions1">
       <p className="instructions">
-        Please wait while while your results are stored.
+        Please wait while while your results are stored. This can take a few minutes.
       </p>
     </div>
   );
